f5db909967332f6292976fed5e38cbc7eb4dc609
JaviDebortoli <javidebortoli@gmail.com>
2026-02-27 11:29:32 -0300

    Primer commit

---
+++ src/test/java/SGI/SgiApplicationTests.java
@@ -0,0 +1,13 @@
+package SGI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
