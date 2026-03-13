13ec0bfad7b4d92630307ddfe8367562b2926d96
JaviDebortoli <javidebortoli@gmail.com>
2026-03-13 12:02:00 -0300

    Auth como variables de entorno y correcciones menores

--- src/main/java/SGI/config/SecurityConfig.java
+++ src/main/java/SGI/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package SGI.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,11 +15,17 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Value("${sgi.security.username}")
+    private String username;
+
+    @Value("${sgi.security.password}")
+    private String password;
+
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails user = org.springframework.security.core.userdetails.User
-                .withUsername("user")
-                .password(passwordEncoder.encode("1234"))
+                .withUsername(username)
+                .password(passwordEncoder.encode(password))
                 .roles("USER")
                 .build();
 
@@ -28,12 +36,12 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/error").permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
 
